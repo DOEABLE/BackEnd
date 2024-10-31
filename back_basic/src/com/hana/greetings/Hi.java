b031673cc91824a9e0ee49df01a2bb02fad39597
KIM DOHEE <kimdh090588@gmail.com>
2024-10-31 17:10:10 +0900

    grade&for-each

--- back_basic/src/com/hana/greetings/Hi.java
+++ back_basic/src/com/hana/greetings/Hi.java
@@ -1,10 +1,30 @@
 package com.hana.greetings;
 
+import org.w3c.dom.ls.LSOutput;
+
+import com.sun.source.tree.SwitchTree;
+
 public class Hi {
 		public static void main(String[] args) {
-				System.out.println("'Hi i'M DOhee");
-				for (int i = 0; i < 4; i++) {
-						System.out.println(i);
+				playSwitch();
+		}
+
+		private static void playSwitch() {
+
+				int score = System.currentTimeMillis() % 2 == 0 ? 81 : 70;
+				final String grade = score > 80 ? "A" : "C";
+				switch (grade) {
+						case "A":
+								System.out.println('1');
+								break;
+						case "B":
+								System.out.println('2');
+								break;
+						case "C":
+								System.out.println('3');
+								break;
+						default:
+								System.out.println("defalut!!");
 				}
 		}
 }
--- back_basic/src/trythis/MyScan.java
+++ back_basic/src/trythis/MyScan.java
@@ -11,17 +11,33 @@ public class MyScan {
 		}
 
 		private static void scanTemp() {
-				final double currTemp = 20;//변하지 않는 수온의 값
 				Scanner scan = new Scanner(System.in);
-				System.out.println("수심을 입력하시오 :");
-				double dept = scan.nextDouble();
-				double resultTemp = currTemp - Math.floor((double)dept / 10) * 0.7;
-				System.out.println(resultTemp);
+				System.out.println("점수 :");
+				int score = scan.nextInt();
 
-		}
+				int grade = score / 10;
+				switch (grade) {
+						case 10:
+						case 9:
+								System.out.println("A: GOAT");
+								break;
+						case 8:
+								System.out.println("B: 잘했어요");
+								break;
+						case 7:
+								System.out.println("C: 나쁘진 않군");
+								break;
+						case 6:
+								System.out.println("D: 좀 더 노력해");
+								break;
+						case 5, 4, 3, 2, 1, 0:
+								System.out.println("F: 다음학기에 또 봐요~");
+								break;
+						default:
+								System.out.println("점수는 0부터 100사이여야 합니다.");
+								break;
 
-		private static Scanner scanUserInfo() {
-				Scanner scan = new Scanner(System.in);
-				return scan;
+				}
+
 		}
 }
