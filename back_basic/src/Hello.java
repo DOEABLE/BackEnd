8ff5a4a9db0fb32f5ccfc8ce78ced1116cbb699e
DOEABLE <kimdh090588@gmail.com>
2024-10-31 22:52:02 +0900

    for-each 공부중

--- back_basic/src/Hello.java
+++ back_basic/src/Hello.java
@@ -1,6 +1,18 @@
 public class Hello {
-	public static void main(String[ ] args){
-		System.out.println("10/31 git test중");
-	}
+    public static void main(String[] args) {
+        String[] colors = new String[]{"blue", "red", "green"};
+
+        //데이터가 있는 만큼 반복(for 문)
+        for (int i = 0; i < colors.length; i++) {
+            System.out.println(colors[i]);
+        }
+
+        //for-each : 데이터가 있는 만큼 반복
+        for (String s : colors) {
+            System.out.println(s);
+
+        }
+
+    }
 
 }
