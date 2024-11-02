ce7bd6caf83076ee3b5261250405eeb47874a33f
DOEABLE <kimdh090588@gmail.com>
2024-11-02 13:34:35 +0900

    계좌정보 입력하고 거래하기

--- back_basic/src/oop/Account.java
+++ back_basic/src/oop/Account.java
@@ -2,74 +2,96 @@ package oop;
 
 import java.util.Scanner;
 
+/**
+ * 1. account class를 만든다
+ * 2. class 내에 주어진 멤버변수를 만든다.(private으로)
+ * 3. 생성자에 객체 생성시 멤버변수(계좌정보는 변할 수 없는 정보입니다.)를 받고(this.으로 현재 객체의 매개변수임을 구분)
+ * 4. main함수 내에 입금을 할지 출금을 할지 선택.
+ * last: 입금(금액 입력 받기), 출금, display 순으로 출력.
+ */
 public class Account {
+    private static Scanner scan = new Scanner(System.in);
+    private final double accountNo;
+    private final String accountOwner;
+    private double balance;
 
-		private int accountNo;
-		private String name;
-		private double balance;
+    ////alt + insert -> constructor
+    public Account(double accountNo, String accountOwner, double balance) {
+        this.accountNo = accountNo; //accountNo 초기화
+        this.accountOwner = accountOwner;
+        this.balance = balance;
+    }
 
-		////alt + insert -> constructor
-		public Account(int accountNo, String name, double balance) {
-				this.accountNo = accountNo;
-				this.name = name;
-				this.balance = balance;
-		}
+    public static void main(String[] args) {
+        //계좌 정보 입력
+        System.out.print("계좌번호 입력:");
+        double accountNo = scan.nextDouble();
+        scan.nextLine();
+        System.out.print("이름:");
+        String accountOwner = scan.nextLine();
+        System.out.print("초기 잔액:");
+        double balance = scan.nextDouble();
+        Account account = new Account(accountNo, accountOwner, balance);
 
-		public static void main(String[] args) {
-				Scanner scan = new Scanner(System.in);
+        System.out.println("어떤 거래를 원하세요?(입금:+, 출금:-): ");
+        char transactionType = scan.next().charAt(0);
+        switch (transactionType) {
+            case '+'://account.deposit매서드 호출
+                account.deposit();
+                break;
+            case '-'://account.withdraw매서드 호출
+                account.withdraw(3000);
+                break;
+        }
+//        account.deposit();
+//        account.withdraw(3000);
+        account.display();
+    }
 
-				//계좌 정보 입력
-				System.out.print("계좌번호 입력:");
-				int accountNo = scan.nextInt();
-				scan.nextLine();
-				System.out.print("이름:");
-				String name = scan.nextLine();
-				System.out.print("잔액:");
-				double balance = scan.nextDouble();
+    public void checkBalance() {//잔액이 거래하기에 유효한지
+        if (balance <= 0) {
+            System.out.println("잔액을 확인하세요.");
+        } else {
+            System.out.printf("%S님의 잔액: %.1f원 입니다.\n", this.accountOwner, this.balance);
+        }
+    }
 
-		}
+    public void deposit() {//입금내역 매서드
+        System.out.println("입금금액을 입력하세요: ");
+        double amount = scan.nextDouble();
+        if (amount > 0) {
+            this.balance += amount;
+            System.out.printf("%.1f원이 입금되었습니다.", amount);
+            this.checkBalance();
+        } else {
+            System.out.println("입금액은 0보다 커야합니다.");
+        }
 
-		public void checkbalance() {
-				if (balance <= 0) {
-						System.out.println("잔액을 확인하세요.");
-				} else {
-						System.out.println("잔액:" + balance);
-				}
-		}
+    }
 
-		public void deposit(double amount) {//입금내역 매서드
-				if (amount > 0) {
-						balance += amount;
-						System.out.println(amount + "원이 입금되었습니다.");
-						this.checkbalance();
-				} else {
-						System.out.println("입금액은 0보다 커야합니다.");
-				}
+    public void withdraw(double amount) {// 출금매서드
+        if (balance >= amount) {
+            this.balance -= amount;
+            System.out.println(amount + "원이 출금되었습니다. 잔액: " + this.balance);
+            this.checkBalance();
+        } else {
+            System.out.println("출금 금액 부족");
+            return;
+        }
+    }
 
-		}
-
-		public void withdraw(double amount) {// 출금매서드
-				if (balance >= amount) {
-						System.out.println(amount + "원이 출금되었습니다. 잔액: " + balance);
-						this.checkbalance();
-				} else {
-						System.out.println("잔액을 확인하세요.");
-				}
-		}
-
-		public void display() {
-				String output = """
-						------------------------
-						계좌번호: %d
-						예금주: %s
-						잔액: %f원
-						------------------------
-												
-						""".formatted(this.accountNo, this.name, this.balance);
-
-				final int hyphenCnt = 15;
-				System.out.println("-".repeat(hyphenCnt));
-				System.out.println(name + "님" + "계좌번호:" + accountNo + "의 잔액은" + balance + "입니다.");
-				System.out.println(output);
-		}
+    public void display() {
+        String output = """
+                ------------------------
+                계좌번호: %.0f
+                예금주: %s
+                잔액: %.1f원
+                ------------------------
+                						
+                """.formatted(this.accountNo, this.accountOwner, this.balance);
+        final int hyphenCnt = 15;
+        System.out.println("-".repeat(hyphenCnt));
+        System.out.printf("%s님의 계좌 %.0f의 잔액은 %.1f입니다.", this.accountOwner, this.accountNo, this.balance);
+        System.out.println(output);
+    }
 }
--- back_basic/src/oop/SuperPerson.java
+++ back_basic/src/oop/SuperPerson.java
@@ -3,54 +3,57 @@ package oop;
 import java.util.Objects;
 
 public class SuperPerson {
-		public static void main(String[] args) {
-				Person hong = new Person("Hong", 32);
-				System.out.println(hong);
-				Person kim = new Person("dohee", 29);
-				System.out.println(kim);
-		}
+    public static void main(String[] args) {
+        Person hong = new Person("Hong", 33);
+        System.out.println("hong = " + hong);
+        Person kim = new Person("Kim", 30);
+        System.out.println("kim = " + kim);
+        System.out.println("age=" + kim.getAge());
+    }
 }
 
 class Person {
-		private final String name;//final은 더이상 상속할 수 없는 const같은 것.
-		private int age;
+    private final String name;
+    private int age;
 
-		public Person(String name, int age) {
-				this.name = name;
-				this.age = age;
-		}
+    public Person(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
 
-		//make getter(읽기) setter
-		public String getName() {
-				return name;
-		}
+    public String getName() {
+        return name;
+    }
 
-		public int getAge() {
-				return age;
-		}
+    public int getAge() {
+        return age;
+    }
 
-		public void setAge(int age) {
-				this.age = age;
-		}
+    public void setAge(int age) {
+        this.age = age;
+    }
 
-		@Override
-		public boolean equals(Object obj) {
-				if (this == obj)
-						return true;
-				if (obj == null || getClass() != obj.getClass())
-						return false;
-				Person person = (Person)obj;
-				return age == person.age && Objects.equals(name, person.name);
-		}
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null || getClass() != obj.getClass()) {
+            return false;
+        }
+        Person person = (Person) obj;
+        return age == person.age && Objects.equals(name, person.name);
+    }
 
-		@Override
-		public int hashCode() {
-				return Objects.hash(name, age);
-		}
+    @Override
+    public int hashCode() {
+        System.out.println("age=" + age);
+        return Objects.hash(name, age);
+    }
 
-		@Override
-		public String toString() {
-				return "Person{name='" + name + '\'' + ", age=" + age + '}';
-		}
-
+    @Override
+    public String toString() {
+        return "Person{"
+                + "name='" + name + '\'' + ", age=" + age + '}';
+    }
 }
